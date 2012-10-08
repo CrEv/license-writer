9ea0e7c774cad53676df743d706ed6407d710872
CrEv <yves.brissaud@gmail.com>
2012-10-08 22:45:12 +0200

    Add: allow to remove old license text before adding new

--- gpl-writer.rb
+++ gpl-writer.rb
@@ -2,37 +2,62 @@
 
 # /!\ the program doesn't check if the license is yet added!
 # Don't execute it 2 times.
-#
-# next: remove old licenses
 
 licensefile = "license.txt"
 
 license = IO.readlines(licensefile)
 
 comments = Hash[
-	"apps/**/*.rb" => ["# ", ""],
-	"apps/**/*.coffee" => ["# ", ""],
-	"apps/**/*.sass" => ["start" => "// ", "end" => ""],
-	"apps/**/*.scss" => ["start" => "// ", "end" => ""],
-	"apps/**/*.js" => ["// ", ""],
-	"apps/**/*.haml" => ["-# ", ""],
-	"apps/**/*.mustache" => ["{{# ", " }}"]
+	"apps/**/*.rb"       => Hash["start" => "# ",   "end" => "",    "skip" => "oldlicense.txt"],
+	"apps/**/*.coffee"   => Hash["start" => "# ",   "end" => "",    "skip" => nil],
+	"apps/**/*.sass"     => Hash["start" => "// ",  "end" => "",    "skip" => nil],
+	"apps/**/*.scss"     => Hash["start" => "// ",  "end" => "",    "skip" => nil],
+	"apps/**/*.js"       => Hash["start" => "// ",  "end" => "",    "skip" => nil],
+	"apps/**/*.haml"     => Hash["start" => "-# ",  "end" => "",    "skip" => nil],
+	"apps/**/*.mustache" => Hash["start" => "{{# ", "end" => " }}", "skip" => nil]
 ]
 
-comments.each { |glob, comment|
-	Dir.glob(glob) { |file|
-		File.open("#{file}_tmp", "w") { |f|
-			license.each {|line|
-				f.write("#{comment[0]}#{line.chop}#{comment[1]}\n")
-			}
+comments.each do |glob, comment|
+	puts(" - check #{glob}")
+
+	oldlicense = []
+	oldlicense = IO.readlines(comment["skip"]) unless comment["skip"] == nil
+
+	Dir.glob(glob) do |file|
+		print(file.ljust(70))
+		File.open("#{file}_tmp", "w") do |f|
+			fileContent = IO.readlines(file)
+			print(".")
+
+			skip = oldlicense.length > 0
+			i = 0
+			oldlicense.each do |line|
+				if line != fileContent[i]
+					skip = false
+					i = 0
+					break
+				end
+				i += 1
+			end
+
+			license.each do |line|
+				f.write("#{comment['start']}#{line.chop}#{comment['end']}\n")
+			end
+			print(".")
 			f.write("\n")
 			f.write("\n")
-			IO.readlines(file).each {|line|
-				f.write(line)
-			}
-		}
+			if skip
+				print("X")
+			end
+			until i > fileContent.length
+				f.write(fileContent[i])
+				i += 1
+			end
+			print(".")
+		end
 		File.delete(file)
+		print(".")
 		File.rename("#{file}_tmp", file)
-		puts("#{file} patched")
-	}
-}
+		puts(".")
+	end
+end
